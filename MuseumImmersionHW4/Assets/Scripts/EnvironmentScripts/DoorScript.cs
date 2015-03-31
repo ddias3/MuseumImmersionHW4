2dbc73ae09d521550b1bbe5fc09c063d16ee7253
Daniel Dias <danieldias15@gmail.com>
2015-03-31 17:18:28 -0400

    Added HW4 package. Created character controller.

---
+++ MuseumImmersionHW4/Assets/Scripts/EnvironmentScripts/DoorScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour
+{
+	public float rotateSpeed;
+	public float rotateAmount;
+
+	public Transform doorTransform;
+	public Collider frontTrigger;
+	public Collider backTrigger;
+
+	public int state;
+
+	private float currentDegree;
+
+	void Update()
+	{
+		switch (state)
+		{
+		case -1:
+
+			break;
+		case 0:
+			break;
+		case 1:
+			break;
+		}
+	}
+}
