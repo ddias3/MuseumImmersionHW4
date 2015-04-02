889e53908f9a432b156be5b0e4c81463b2b2ccb8
Spex130 <Gamerguy56@bellsouth.net>
2015-04-01 22:32:14 -0400

    Collision Sounds created.
    When you bump into things, they make sound. To make this work, add a
    Trigger collider to your object (you can handle normal collision using a
    Mesh Collider). Right now it has to be a bit bigger than the object.
    Working on that.

---
+++ MuseumImmersionHW4/Assets/Scripts/CollisionSounds/collisionNoiseCheck.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisionNoiseCheck : MonoBehaviour {
+
+	public AudioSource mySource;
+
+	public AudioClip wallClip;	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.tag == "Wall"){
+			mySource.clip = wallClip;
+			mySource.Play();
+			print("WALL COLLISION");
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Wall"){
+			mySource.clip = wallClip;
+			mySource.Play();
+			print("WALL COLLISION");
+		}
+	}
+
+}
