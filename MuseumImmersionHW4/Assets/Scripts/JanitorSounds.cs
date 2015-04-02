1a782e47183b02de57456cace60993437839c509
Michael Dunn <mdunn31@gmail.com>
2015-04-02 17:33:12 -0400

    My changes for moving stuff around
    Yay~

---
+++ MuseumImmersionHW4/Assets/Scripts/JanitorSounds.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JanitorSounds : MonoBehaviour {
+
+	int timer;
+	AudioSource janitorSource;
+	public AudioClip[] janitorClips;
+	int track;
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+		track = 0;
+		janitorSource = GameObject.Find("Janitor").GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer%20 == 0){
+			janitorSource.clip = janitorClips[track];
+			janitorSource.Play();
+			track++;
+		}
+		timer ++;
+		if(timer >= 80){
+			timer = 0;
+			track = 0;
+		}
+	}
+}
