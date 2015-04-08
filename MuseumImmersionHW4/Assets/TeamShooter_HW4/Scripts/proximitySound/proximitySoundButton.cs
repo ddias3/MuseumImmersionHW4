e094533d5694bdc5b4231ff72bd8eb3f4e88b9e5
Spex130 <Gamerguy56@bellsouth.net>
2015-04-07 20:56:30 -0400

    LocationAlert complete
    There's an example in my TestScene

---
+++ MuseumImmersionHW4/Assets/TeamShooter_HW4/Scripts/proximitySound/proximitySoundButton.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class proximitySoundButton : MonoBehaviour {
+
+
+	public float timer = 1f;
+
+	public bool shouldTimer = false;
+
+	public bool shouldPlay = false;
+
+	public int arrayInt = 0;
+
+	public proximitySoundNode[] nodeArray = new proximitySoundNode[1];
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void buttonUpdate(){
+		if(shouldTimer){
+			if(timer > 0){
+				timer-= Time.deltaTime;
+			}
+			else{
+				timer = 1f;
+				shouldPlay = true;
+			}
+		}
+		if(shouldPlay){
+			if(arrayInt >= nodeArray.Length){
+				shouldPlay = false;
+				shouldTimer = false;
+				arrayInt = 0;
+			}
+			else{
+				nodeArray[arrayInt].playTierSound();
+				shouldPlay = false;
+				arrayInt++;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp (KeyCode.Q)){
+			shouldTimer = true;
+		}
+		buttonUpdate();
+	}
+
+	public void playCheck(){
+		for(int i = 0; i < nodeArray.Length; i++){
+			if(!(nodeArray[i].isVisited())){
+				nodeArray[i].playTierSound();
+			}
+			float f = 2f;
+		}
+	}
+
+}
---
+++ MuseumImmersionHW4/Assets/TeamShooter_HW4/Scripts/proximitySound/proximitySoundNode.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class proximitySoundNode : MonoBehaviour {
+
+	public GameObject player;
+	public float distance;
+	public float playerThres = 4f;
+	public bool hasBeenVisited = false;
+
+	public int soundTier = 1;
+
+	public int distTier1 = 25;
+
+	public int distTier2 = 50;
+
+	public int distTier3 = 75;
+
+
+	public AudioSource tier0Sound;
+	public AudioSource tier1Sound;
+	public AudioSource tier2Sound;
+	public AudioSource tier3Sound;
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance = Vector3.Distance(transform.position, player.transform.position);
+		if(distance <= playerThres){
+			hasBeenVisited = true;
+		}
+		else{
+			if(distance < distTier1){
+				soundTier = 1;
+			}
+			if(distance > distTier1 && distance < distTier2){
+				soundTier = 2;
+			}
+			if(distance > distTier2 && distance < distTier3){
+				soundTier = 3;
+			}
+			if(distance > distTier3){
+				soundTier = 4;
+			}
+		}
+	}
+
+	public bool isVisited(){
+		return hasBeenVisited;
+	}
+
+	public int getDistTier(){
+		return soundTier;
+	}
+
+	public void playTierSound(){
+		if(soundTier == 1){
+			tier0Sound.Play ();
+		}
+		else if(soundTier == 2){
+			tier1Sound.Play ();
+		}
+		else if(soundTier == 3){
+			tier2Sound.Play ();
+		}
+		else if(soundTier == 4){
+			tier3Sound.Play ();
+		}
+	}
+
+}
