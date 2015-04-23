c3ef1713dc43830b40f5a67208bb27855db03e7f
Spex130 <Gamerguy56@bellsouth.net>
2015-04-23 14:38:33 -0400

    Made a Buzzer that works by proximity.
    Sound file's off at the moment, but the code works.

---
+++ MuseumImmersionHW4/Assets/TeamShooter_HW4/Scripts/Popularity/buzzerScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buzzerScript : MonoBehaviour {
+
+	public float timer = 6f;
+	float origTimer = 0f;
+	
+	public AudioSource buzzSource;
+	
+
+	public AudioClip buzzWarn;
+
+	
+	public float distance = 0f;
+	
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		distance = Vector3.Distance(player.transform.position, transform.position);
+		origTimer = timer;
+
+		//player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance = Vector3.Distance(player.transform.position, transform.position);
+		if(timer > 0){
+			timer-= Time.deltaTime;
+		}
+		else{
+			if(distance < 10){
+				buzzSource.clip = buzzWarn;
+				buzzSource.Play();
+				timer = origTimer;
+			}
+		}
+	}
+}
